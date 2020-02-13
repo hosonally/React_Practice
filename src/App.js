93443f20684db32d686fd05b0e6e8d98054c4335
hosonally <60739572+hosonally@users.noreply.github.com>
2020-02-13 20:55:47 +0900

    TodoList
    able to add list, but not delete or edit yet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [fruitList, setFruitList] = useState(['リンゴ', 'バナナ', 'いちご'])
+  const [todoList, setTodoList] = useState(["リンゴ", "バナナ", "いちご"]);
+  const [todoName, setTodoName] = useState("");
+
+  function doAddTodo() {
+    todoList.push(todoName);
+    return todoList;
+  }
+
+  const addTodo = function() {
+    todoList.push(todoName);
+    setTodoName("");
+  };
+
+  const inputtedName = function(e) {
+    setTodoName(e.target.value);
+  };
+
+  const editTodo = function() {};
 
   return (
     <div>
-      {fruitList.map(function(fruit) {
+      <input type="text" onInput={inputtedName} value={todoName} />
+      ：TODO <button onClick={addTodo}>追加</button>
+      {todoList.map(function(todo) {
         return (
-          <div>{fruit}食べるぜ</div>
-        )
+          <div>{todo}食べるぜ</div>,
+          <button onClick="">編集</button>,
+          <button onClick="">削除</button>
+        );
       })}
     </div>
   );
